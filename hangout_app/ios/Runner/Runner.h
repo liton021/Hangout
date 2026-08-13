b01876233f99f0d6fcbe7851d367ba02996384b5
liton021 <163072684+liton021@users.noreply.github.com>
2026-08-13 13:29:55 +0000

    fix: add missing Firebase/Android/iOS project files for CI build
    Co-authored-by: kiloconnect[bot] <240665456+kiloconnect[bot]@users.noreply.github.com>

---
+++ hangout_app/ios/Runner/Runner.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface AppDelegate : FlutterAppDelegate
+@end
---
+++ hangout_app/ios/Runner/main.m
@@ -0,0 +1,12 @@
+#include "FlutterGeneratedPluginRegistrant.h"
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
